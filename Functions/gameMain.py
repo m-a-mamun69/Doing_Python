2bd0aa6965262c9987d3f167ec7d254f90066285
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2024-12-21 12:21:02 +0600

    Python Packages Stuff.

---
+++ Functions/gameMain.py
@@ -0,0 +1,6 @@
+# import packageGame.characters.player
+from packageGame.characters import player, boss
+
+player.get_player_info()
+boss.get_boss_info()
+# packageGame.characters.player.get_player_info()
