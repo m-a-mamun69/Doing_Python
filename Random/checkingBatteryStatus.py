80eafc456de6fc6ecec85ef91b63faa40213e065
m-a-mamun69 <m.a.mamunrashid69@gmail.com>
2025-01-10 15:29:14 +0600

    Checking Battery Status Using Python

---
+++ Random/checkingBatteryStatus.py
@@ -0,0 +1,18 @@
+import psutil 
+
+battery = psutil.sensors_battery()
+
+if battery is not None:
+    print("Battery Percentage: ", battery.percent, "%")
+
+    print("Power Plugged in.", battery.power_plugged)
+
+    def convertTime(seconds):
+        minutes, seconds = divmod(seconds, 60)
+        hours, minutes = divmod(minutes, 60)
+        return "%d:%02d:%02d" % (hours, minutes, seconds)
+    
+    print("Battery Remaining Time: ", convertTime(battery.secsleft))
+
+else:
+    print("No Battery Information Available.")
