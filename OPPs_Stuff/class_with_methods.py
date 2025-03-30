a5f3eb9f1cb62dc1e13047672629d8401234ecdd
m.a.mamunrashid69@gmail.com <m.a.mamunrashid69@gmail.com>
2025-03-30 16:33:59 +0600

    Class With Methods.

---
+++ OPPs_Stuff/class_with_methods.py
@@ -0,0 +1,14 @@
+
+class Circle():
+	"""docstring for ClassName"""
+	def __init__(self, rarius):
+		self.rarius = rarius
+
+	def aera(self):
+		return 3.1416 * self.rarius ** 2
+
+
+class_obj = Circle(5)
+cal_aera = round(class_obj.aera())
+print("Area is: ", cal_aera)
+		
